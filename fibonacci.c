c7f9386f6cf6bdf3715e9af5620401c198824c8a
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-12 23:54:08 +0200

    Adding docstrings explaining why naive brute force is bad.

--- fibonacci.c
+++ fibonacci.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 
 
+/* naive_fibonacci
+*  The naive fibonacci is the classic exercise when it comes to recursions. It has, however, an exponential time complexity and is very slow. Consider for instance a big problem instance
+*  that is, n is a very large number. Consider even fibonacci 8, fib(8) = fib(7) + fib(6) = fib(6) + fib(5) + fib(5) + fib(4) = fib(5) + fib(4) + 2(fib(4) + fib(3) + fib(3) + fib(2) ...
+*  The problem here is overlaps, we get a large amount of overlaps for large n.
+*/
 
 int naive_fibonacci(int n){
     if(n == 1){
@@ -16,6 +21,10 @@ int naive_fibonacci(int n){
     }
 }
 
+/* bot_up_fibonacci
+*  This implementation is the cure for the bad timecomplexity above and it is, as a matter of fact, linear in time complexity while only being slightly more complicated. See, if we
+*  now want fib(8) we will work our way up, starting with fib(3) = 1 + 1 -> fib(4) = 2 + 1, fib(5) = 3 + 2 -> ... fib(8) = fib(7) + fib(6). 
+*/
 
 int bot_up_fibonacci(int n){
     int n1 = 1;
